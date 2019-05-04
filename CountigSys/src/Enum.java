f1cbff38b449b9b9c5bcc6645ce7270969f17fe8
F4NNI <50243591+F4NNI@users.noreply.github.com>
2019-05-04 15:16:31 +0300

    Add files via upload

--- CountigSys/src/Enum.java
+++ CountigSys/src/Enum.java
@@ -15,63 +15,16 @@ public class Enum
 		String sCur = in.readLine();
 		int a = Integer.parseInt(sCur);
 		
-		if(a == 2)
-		{
-			
-			System.out.println("Enter Your count: ");
-			String sNew = in.readLine();
-			int x = Integer.parseInt(sNew,a);
-			
-			System.out.println("Enter base of target number system: " );
-			String sNu = in.readLine();
-			int b = Integer.parseInt(sNu);
-			
-			System.out.println("Your count: " + Integer.toString(x , b));
-		}
+		System.out.println("Enter Your count: ");
+		String sNew = in.readLine();
+		int x = Integer.parseInt(sNew,a);
 		
-		if(a >= 3)
-		{
-			
-			if(a == 16) 
-			{
-				System.out.println("Enter your count: ");
-				String sNum = in.readLine();
-				int x = Integer.parseInt(sNum, a);
-				
-				System.out.println("Enter base of target number system: " );
-				String sNu = in.readLine();
-				int b = Integer.parseInt(sNu);
-				
-				System.out.println("Your count: " + Integer.toString(x , b));
-			}
-			
-			if (a == 8)
-			{
-				System.out.println("Enter your count: ");
-				String sNum = in.readLine();
-				int x = Integer.parseInt(sNum, a);
-				
-				System.out.println("Enter base of target number system: " );
-				String sNu = in.readLine();
-				int b = Integer.parseInt(sNu);
-				
-				System.out.println("Your count: " + Integer.toString(x , b));
-			}
-			
-			else
-			{
-				
-				System.out.println("Enter your count: ");
-				String sNum = in.readLine();
-				int x = Integer.parseInt(sNum,a);
-				
-				System.out.println("Enter base of target number system: " );
-				String sNu = in.readLine();
-				int b = Integer.parseInt(sNu);
-				
-				System.out.println("Your count: " + Integer.toString(x , b));
-			}
-		}
+		System.out.println("Enter base of target number system: " );
+		String sNu = in.readLine();
+		int b = Integer.parseInt(sNu);
+		
+		System.out.println("Your count: " + Integer.toString(x , b));
+		
 	}
 	
 }
