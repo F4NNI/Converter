5d1977841f947f476dbde64b7d3ff2b347e496d8
F4NNI <50243591+F4NNI@users.noreply.github.com>
2019-05-08 16:59:00 +0300

    Add files via upload

--- CountigSys/src/Enum.java
+++ CountigSys/src/Enum.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 
 public class Enum 
 {
-		
+	
 	public static void main(String[] args) throws IOException
 	{
 		
@@ -15,6 +15,12 @@ public class Enum
 		String sCur = in.readLine();
 		int a = Integer.parseInt(sCur);
 		
+		if (a < 2 || a >= 37)
+		{
+			System.out.println("Error!");
+			in.skip(a);
+		}
+		
 		System.out.println("Enter Your count(x): ");
 		String sNew = in.readLine();
 		int x = Integer.parseInt(sNew,a);
@@ -22,6 +28,14 @@ public class Enum
 		System.out.println("Enter base of target number system(b): " );
 		String sNu = in.readLine();
 		int b = Integer.parseInt(sNu);
+		
+		if (b < 2 || b >= 37)
+		{
+			System.out.println("Error!");
+			in.skip(b);
+		}
+		
+		
 		
 		System.out.println("Your count(x): " + Integer.toString(x , b)+ "  " + "on the bases: " + b);
 		
