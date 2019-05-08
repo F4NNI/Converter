d266c6b0bd14629aa0c3ff3855d686fb16900631
F4NNI <50243591+F4NNI@users.noreply.github.com>
2019-05-08 14:35:07 +0300

    Add files via upload

--- CountigSys/src/Enum.java
+++ CountigSys/src/Enum.java
@@ -11,19 +11,19 @@ public class Enum
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		System.out.println("Enter base of original number system: ");
+		System.out.println("Enter base of original number system(a): ");
 		String sCur = in.readLine();
 		int a = Integer.parseInt(sCur);
 		
-		System.out.println("Enter Your count: ");
+		System.out.println("Enter Your count(x): ");
 		String sNew = in.readLine();
 		int x = Integer.parseInt(sNew,a);
 		
-		System.out.println("Enter base of target number system: " );
+		System.out.println("Enter base of target number system(b): " );
 		String sNu = in.readLine();
 		int b = Integer.parseInt(sNu);
 		
-		System.out.println("Your count: " + Integer.toString(x , b));
+		System.out.println("Your count(x): " + Integer.toString(x , b)+ "  " + "on the bases: " + b);
 		
 	}
 	
